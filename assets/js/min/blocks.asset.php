ba8d61a782e54fe539328288a53ff93eb17a9bcc
Mike Hemberger <mike@thestizmedia.com>
2021-03-31 17:34:12 -0400

    fix missing wp.i18n

--- assets/js/min/blocks.asset.php
+++ assets/js/min/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '9ce1f575d3a319a986449998fcb4d898');
+<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => '45b94d5fe3a515d5bbc96a343b726bd2');
